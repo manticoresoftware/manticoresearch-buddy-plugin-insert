3375ba512da15429804d6929a87328b87cb71afc
nick <nick@manticoresearch.com>
2024-01-08 18:01:53 +0700

    Fixed a typo

--- test/InsertQuery/InsertQueryHandlerTest.php
+++ test/InsertQuery/InsertQueryHandlerTest.php
@@ -51,7 +51,7 @@ class InsertQueryHandlerTest extends TestCase {
 			['key' => 'searchd.pid_file', 'value' => '/var/run/manticore/searchd.pid'],
 			['key' => 'searchd.data_dir', 'value' => '/var/lib/manticore'],
 			['key' => 'searchd.query_log_format', 'value' => 'sphinxql'],
-			['searchd.buddy_path', 'value' => 'manticore-executor /workdir/src/ main.php --debug'],
+			['key' => 'searchd.buddy_path', 'value' => 'manticore-executor /workdir/src/ main.php --debug'],
 			['key' => 'common.plugin_dir', 'value' => '/usr/local/lib/manticore'],
 			['key' => 'common.lemmatizer_base', 'value' => '/usr/share/manticore/morph/'],
 			]
